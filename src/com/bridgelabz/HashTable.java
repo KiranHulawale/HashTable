4688c136c201dca4c9da635a7dc093fa70159f20
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-03-28 12:31:43 +0530

    Update HashTable.java

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,10 +1,19 @@
 package com.bridgelabz;
 
-public class HashTable {
+public class HashTable{
     public static void main(String[] args) {
-        String str = "To be or not to be";
 
-        HashMapTest hash = new HashMapTest(6);
+        String str = "Paranoids are not paranoid because "
+                + "they are paranoid but because they keep "
+                + "putting themselves deliberately into paranoid "
+                + "avoidable situations";
+
+        String word = "avoidable";
+
+        HashMapTest hash = new HashMapTest();
         hash.countWords(str);
+        hash.remove(str, word);
     }
+
 }
+
